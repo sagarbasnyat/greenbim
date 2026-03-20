0fa1b5b248ece41bb05dad7e159d40c142834ce3
Sagar Basnet <sagarbasnet@Sagars-MacBook-Air.local>
2026-03-20 16:37:24 +0200

    Update to correct Groq model

--- core/ai_agent.py
+++ core/ai_agent.py
@@ -125,7 +125,7 @@ def get_ai_recommendations(
         )
 
         response = client.chat.completions.create(
-            model="mixtral-8x7b-32768",
+            model="llama-3.3-70b-versatile",
             messages=[
                 {
                     "role": "user",
